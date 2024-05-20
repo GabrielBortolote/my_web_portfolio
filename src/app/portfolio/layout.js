5c9bf2be2cf4b9a384f149ea37d2f12d7a9ae96c
Gabriel Bortolote <gbortolote@gmail.com>
2024-05-20 14:33:56 -0300

    First commit

---
+++ src/app/portfolio/layout.js
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }) {
+  return (
+      <>
+        <h2>{'I\'m the portfolio layout'}</h2>
+        {children}
+      </>
+  )
+}
---
+++ src/app/portfolio/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h3>{'I\'m a portfolio'}</h3>
+}
