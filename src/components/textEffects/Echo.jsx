f1a4cbd100b1ed1aeec481b6b95044f9380d6e44
Gabriel Bortolote <gbortolote@gmail.com>
2024-05-24 17:04:21 -0300

    Fix echo broken effect

--- src/components/textEffects/Echo.jsx
+++ src/components/textEffects/Echo.jsx
@@ -2,10 +2,16 @@ export default function EchoTextEffect({children, color}){
   return <>
     <span className="relative w-full z-0">
       {children}
-      <span className={`absolute left-1 top-1 w-full -z-10 text-[${color}]/50`}>
+      <span className={`absolute left-1 top-1 w-full -z-10`} style={{
+        color: color,
+        opacity: '50%',
+      }}>
         {children}
       </span>
-      <span className={`absolute left-2 top-2 w-full -z-20 text-[${color}]/25`}>
+      <span className={`absolute left-2 top-2 w-full -z-20`} style={{
+        color: color,
+        opacity: '25%',
+      }}>
         {children}
       </span>
     </span>
