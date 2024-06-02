4c03a05c8ae68380ae2e5540f23cd43792d1139e
Gabriel Bortolote <gbortolote@gmail.com>
2024-06-02 14:32:28 -0300

    Add page title and description

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
-import StartView from "@/components/views/start/StartView";
 import AboutView from "@/components/views/about/AboutView";
+import StartView from "@/components/views/start/StartView";
+
+export const metadata = {
+  title: 'Gabriel B',
+  description: 'Gabriel Bortolote Web Place',
+}
 
 export default function Page() {
   return <>
