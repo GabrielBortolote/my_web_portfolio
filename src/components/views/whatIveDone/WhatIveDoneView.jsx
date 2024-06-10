e9d770c6159a6ff20c45173375072995121369bf
Gabriel Bortolote <gbortolote@gmail.com>
2024-06-10 18:27:27 -0300

    What I've done view skeleton

--- src/components/views/whatIveDone/WhatIveDoneView.jsx
+++ src/components/views/whatIveDone/WhatIveDoneView.jsx
@@ -1,5 +1,24 @@
 export default function WhatIveDoneView({dict}){
-  return <>
-    cu
-  </>
+  return <div className="
+    flex flex-row
+    flex-wrap lg:flex-nowrap
+    text-light
+  ">
+    <div className="
+      flex flex-col
+      flex-1
+      items-end
+      border-red
+      border-2
+    ">
+      Porfolio
+    </div>
+    <div className="
+      flex-1
+      border-red
+      border-2
+    ">
+      Picture
+    </div>
+  </div>
 }
